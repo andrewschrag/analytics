d5bcfe59b94d950b58f652917173185c66e473c3
Andrew Schrag <50116623+andrewschrag@users.noreply.github.com>
2023-07-26 16:37:52 -0700

    Update cohort_func.R

--- cohort_func.R
+++ cohort_func.R
@@ -5,7 +5,7 @@ get_cohort <-
   function(cohort,
            cohort_type = 'Curated',
            schema = 'mdr',
-           con = spmd_con('prod'), 
+           con = spmd_con(), 
            cols = c()) {
     tbl(con, in_schema("cohorts", "cohort")) %>%
       filter(tolower(name) == tolower(cohort),
