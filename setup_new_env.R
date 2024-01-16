beca837561724d35fa994c310a4e7f67ba100530
Andrew Schrag <50116623+andrewschrag@users.noreply.github.com>
2024-01-16 14:34:01 -0800

    Update setup_new_env.R

--- setup_new_env.R
+++ setup_new_env.R
@@ -1,38 +1,37 @@
 ## Script for installing packages into new renv
-setup_new_env <- function(){ 
-  options(install.packages.compile.from.source = "always")
-  install.packages(c('pacman', 'devtools'))
-  devtools::install_github('https://github.com/cran/quantreg')
-  devtools::install_github('https://github.com/jackwasey/icd')
-  pacman::p_load(
-      'devtools',
-      'syhelpr',
-      'tidyverse',
-      'dbplyr',
-      'dtplyr',
-      'plotly',
-      'glue',
-      'janitor',
-      'vroom',
-      'tidyjson',
-      'tidytext',
-      'gt',
-      'gtsummary',
-      'botor',
-      'labelled',
-      'httr',
-      'jsonlite',
-      'syrnapi',
-      'formatR',
-      'rvest',
-      'RJSONIO',
-      'pdftools',
-      'arrow',
-      'odbc',
-      'paws'
-  )
-  
-  if(!reticulate::condaenv_exists('~/.local/share/r-miniconda/envs/r-reticulate/')){
-    reticulate::install_miniconda(path = '~/.local/share/r-miniconda/')
-  }
-  reticulate::use_miniconda('~/.local/share/r-miniconda/envs/r-reticulate/bin/python') }
+options(install.packages.compile.from.source = "always")
+install.packages(c('pacman', 'devtools'))
+devtools::install_github('https://github.com/cran/quantreg')
+devtools::install_github('https://github.com/jackwasey/icd')
+pacman::p_load(
+  'devtools',
+  'syhelpr',
+  'tidyverse',
+  'dbplyr',
+  'dtplyr',
+  'plotly',
+  'glue',
+  'janitor',
+  'vroom',
+  'tidyjson',
+  'tidytext',
+  'gt',
+  'gtsummary',
+  'botor',
+  'labelled',
+  'httr',
+  'jsonlite',
+  'syrnapi',
+  'formatR',
+  'rvest',
+  'RJSONIO',
+  'pdftools',
+  'arrow',
+  'odbc',
+  'paws'
+)
+
+if (!reticulate::condaenv_exists('~/.local/share/r-miniconda/envs/r-reticulate/')) {
+  reticulate::install_miniconda(path = '~/.local/share/r-miniconda/')
+}
+reticulate::use_miniconda('~/.local/share/r-miniconda/envs/r-reticulate/bin/python')
