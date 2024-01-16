ea9c73d2904e3d79a2343b81702ce94a630206ef
Andrew Schrag <50116623+andrewschrag@users.noreply.github.com>
2024-01-16 14:24:17 -0800

    Create setup_new_env.R

---
+++ setup_new_env.R
@@ -0,0 +1,40 @@
+## Script for installing packages into new renv
+setup_new_env <- function(){ 
+  options(install.packages.compile.from.source = "always")
+  install.packages(c('pacman', 'devtools'))
+  devtools::install_github('https://github.com/cran/quantreg')
+  devtools::install_github('https://github.com/jackwasey/icd')
+  pacman::p_load(
+      'devtools',
+      'syhelpr',
+      'tidyverse',
+      'dbplyr',
+      'dtplyr',
+      'plotly',
+      'glue',
+      'janitor',
+      'vroom',
+      'tidyjson',
+      'tidytext',
+      'gt',
+      'gtsummary',
+      'botor',
+      'labelled',
+      'httr',
+      'jsonlite',
+      'syrnapi',
+      'formatR',
+      'rvest',
+      'RJSONIO',
+      'pdftools',
+      'arrow',
+      'odbc',
+      'paws'
+  )
+  
+  if(!reticulate::condaenv_exists('~/.local/share/r-miniconda/envs/r-reticulate/')){
+    reticulate::install_miniconda(path = '~/.local/share/r-miniconda/')
+  }
+  reticulate::use_miniconda('~/.local/share/r-miniconda/envs/r-reticulate/bin/python')
+}
+
