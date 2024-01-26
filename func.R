4dfc239f81e03cbdb84218cc41b2363c97f4d314
Andrew Schrag <50116623+andrewschrag@users.noreply.github.com>
2024-01-25 16:15:30 -0800

    Update func.R

--- func.R
+++ func.R
@@ -42,7 +42,7 @@ make_table <- function (df,
       sort = sort,
       ...,
       type = all_continuous() ~ "continuous2",
-      statistic = ) %>%
+      statistic = statistic) %>%
     suppressMessages() %>%
     modify_header(all_stat_cols() ~ "**{level}**, N={n} ({style_percent(p)}%)",
                   label = .label) %>%
