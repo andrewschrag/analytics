4483305e81e34f2c068b75fbf4f7eac349277b3e
Andrew Schrag <50116623+andrewschrag@users.noreply.github.com>
2025-08-07 09:06:22 -0700

    Update init.R

--- init.R
+++ init.R
@@ -43,21 +43,21 @@ plot_margin <-
 
 
 #This is the connection config
-rds_client = botor_client("rds", region_name = "us-west-2")
-host = host = 'spmd-prod.cluster-czsq80p56jgd.us-west-2.rds.amazonaws.com' #'spmd-prod-clone.cluster-czsq80p56jgd.us-west-2.rds.amazonaws.com'
-port = '5432'
-user = 'view'
-spmd <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  #Driver = "postgresql",
-  #database = "spmd",
-  dbname = 'spmd', 
-  host = host,
-  port = port,
-  user = user,
-  password = rds_client$generate_db_auth_token(host, port, user),
-  sslmode = "require"
-)
+#rds_client = botor_client("rds", region_name = "us-west-2")
+#host = host = 'spmd-prod.cluster-czsq80p56jgd.us-west-2.rds.amazonaws.com' #'spmd-prod-clone.cluster-czsq80p56jgd.us-west-2.rds.amazonaws.com'
+#port = '5432'
+#user = 'view'
+#spmd <- DBI::dbConnect(
+#  RPostgres::Postgres(),
+#  #Driver = "postgresql",
+#  #database = "spmd",
+#  dbname = 'spmd', 
+#  host = host,
+#  port = port,
+#  user = user,
+#  password = rds_client$generate_db_auth_token(host, port, user),
+#  sslmode = "require"
+#)
 
 shared_root = 'https://raw.githubusercontent.com/andrewschrag/analytics/main'
 source(file.path(shared_root, 'func.R'))
